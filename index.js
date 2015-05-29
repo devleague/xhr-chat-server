2ceeb069bb0138fe527824edf85c2ca3dc7aa30e
sgnl <rayrfarias@gmail.com>
2015-05-28 18:27:42 -1000

    add validation POST /{chatroom_name}

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var Hapi  = require('hapi');
-var Fs    = require('fs');
-var Path  = require('path');
+var Hapi  = require( 'hapi' );
+var Joi   = require( 'joi' );
+var Fs    = require( 'fs' );
+var Path  = require( 'path' );
 
-var dbPath = Path.join(process.cwd(), '/database/chatrooms.json');
+var dbPath = Path.join( process.cwd(), '/database/chatrooms.json' );
 
 var server = new Hapi.Server({
   connections: {
@@ -24,8 +25,8 @@ server.route({
   handler: getMessagesByChatroomId
 });
 
-function getMessagesByChatroomId(request, reply){
-  var db = getDatabase(request.params.id);
+function getMessagesByChatroomId( request, reply ) {
+  var db = getDatabase( request.params.id );
 
   reply( db[request.params.id].messages );
 }
@@ -33,11 +34,19 @@ function getMessagesByChatroomId(request, reply){
 server.route({
   method: 'POST',
   path: '/{id}',
-  handler: setNewMessageByChatroomId
+  handler: setNewMessageByChatroomId,
+  config: {
+    validate: {
+      payload: {
+        name: Joi.string().trim().alphanum().min( 3 ).max( 16 ),
+        message: Joi.string().trim().min( 12 )
+      }
+    }
+  }
 });
 
-function setNewMessageByChatroomId(request, reply){
-  var db = getDatabase(request.params.id);
+function setNewMessageByChatroomId( request, reply ) {
+  var db = getDatabase( request.params.id );
 
   // client sends name and message in payload
   db[request.params.id].messages.push({
@@ -49,29 +58,29 @@ function setNewMessageByChatroomId(request, reply){
   });
 
   // save the file
-  console.log(dbPath);
-  Fs.writeFile('./database/chatrooms.json', JSON.stringify( db ), function (err) {
-    if (err) return console.log(err);
-    console.log('new message added to the room: ' + request.params.id);
+  Fs.writeFile( './database/chatrooms.json', JSON.stringify( db ), function( err ) {
+    if ( err ) return console.log( err );
+    console.log( 'new message added to the room: ' + request.params.id );
   });
 
-  reply(db[request.params.id].messages);
+  reply( db[request.params.id].messages );
 }
 
-function getDatabase(roomId){
-  var db = require(dbPath);
+function getDatabase( roomId ) {
+  var db = require( dbPath );
 
-  if (typeof db[roomId] === 'undefined') {
+  if ( typeof db[roomId] === 'undefined' ) {
     db[roomId] = {
       name: roomId,
       messages: []
     };
 
     // save the file since we made changes
-    Fs.writeFile(dbPath, JSON.stringify( db ), function (err) {
-      if (err) return console.log(err);
-      console.log('database was updated with a new room: ' + roomId);
+    Fs.writeFile( dbPath, JSON.stringify( db ), function( err ) {
+      if ( err ) return console.log( err );
+      console.log( 'database was updated with a new room: ' + roomId );
     });
   }
+
   return db;
 }
